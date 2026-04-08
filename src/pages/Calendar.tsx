11e77546c5a369b455ae63fc296d7a1c8c83abd3
it13manoj <it13manoj@gmail.com>
2026-04-08 23:22:26 +0530

    registation

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -173,7 +173,21 @@ const Calendar: React.FC = () => {
               <div>
                 <div>
                   <label className="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
-                    Event Title
+                    Client Name
+                  </label>
+                  <input
+                    id="event-title"
+                    type="text"
+                    value={eventTitle}
+                    onChange={(e) => setEventTitle(e.target.value)}
+                    className="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
+                  />
+                </div>
+              </div>
+              <div>
+                <div>
+                  <label className="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
+                  VANUS
                   </label>
                   <input
                     id="event-title"
